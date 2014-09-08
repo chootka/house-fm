6d903e639bdb292ccb113a59dc736f3d14a03cf4
chootka <com@chootka.com>
2014-09-07 20:17:29 -0400

    removed subnodes submodule; added scripts from mesh-only subnodes branch to off-the-grid scripts

---
+++ scripts/configure.sh
@@ -0,0 +1,30 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# SOFTWARE INSTALL
+#
+# update the packages (may take a long time)
+sudo apt-get update
+#&& sudo apt-get -y upgrade
+
+# install prerequisite software
+sudo apt-get install -y batctl iw
+
+# add the batman-adv module to be started on boot
+sudo sed -i '$a batman-adv' /etc/modules
+sudo modprobe batman-adv
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# NETWORK CONFIGURATION
+#
+# modify the network interface config file
+sudo python scripts/configure_network_interfaces.py
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# CREATE STARTUP SCRIPT
+#
+# copy startup script to init.d
+# subnodes script configures and starts mesh point on boot
+sudo cp scripts/little-nets.sh /etc/init.d/little-nets
+sudo chmod 755 /etc/init.d/little-nets
+sudo update-rc.d little-nets defaults
+sudo /etc/init.d/little-nets restart
